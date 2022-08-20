8c1f9986655dd75180fff821506a8df6321f2b04
tariq-shuvo <tariq10043@gmail.com>
2022-08-20 12:53:57 +0600

    eslint and pretierrc has been configured and implemented

--- config/global.config.js
+++ config/global.config.js
@@ -1,35 +1,35 @@
-const { dotEnvPath } = require('./path/index');
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
+const { dotEnvPath } = require("./path/index");
 
-dotenv.config({path: dotEnvPath()})
+dotenv.config({ path: dotEnvPath() });
 
 module.exports = {
-    cashIn: `${process.env.CASH_IN}`,
-    cashOutNatural: `${process.env.CASH_OUT_NATURAL}`,
-    cashOutJuridical: `${process.env.CASH_OUT_JURIDICAL}`,
-    // If dynamic axios api failed to get data
-    cashInConstant: {
-        percents: 0.03,
-        max: {
-            amount: 5,
-            currency: "EUR"
-        }
+  cashIn: `${process.env.CASH_IN}`,
+  cashOutNatural: `${process.env.CASH_OUT_NATURAL}`,
+  cashOutJuridical: `${process.env.CASH_OUT_JURIDICAL}`,
+  // If dynamic axios api failed to get data from url .env file
+  cashInConstant: {
+    percents: 0.03,
+    max: {
+      amount: 5,
+      currency: "EUR",
     },
-    // If dynamic axios api failed to get data
-    cashOutConstant: {
-        natural: {
-            percents: 0.3,
-            week_limit: {
-                amount: 1000,
-                currency: "EUR"
-            }
-        },
-        juridical: {
-            percents: 0.3,
-            min: {
-                amount: 0.5,
-                currency: "EUR"
-            }
-        }
-    }
-}
+  },
+  // If dynamic axios api failed to get data from url .env file
+  cashOutConstant: {
+    natural: {
+      percents: 0.3,
+      week_limit: {
+        amount: 1000,
+        currency: "EUR",
+      },
+    },
+    juridical: {
+      percents: 0.3,
+      min: {
+        amount: 0.5,
+        currency: "EUR",
+      },
+    },
+  },
+};
